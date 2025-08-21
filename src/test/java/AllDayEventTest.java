3b127fbee5d50d28ce4e8230e88b0e39826b01df
heecong <yongcowhee@gmail.com>
2025-08-21 12:12:41 +0900

    feat: future all day event create test

--- src/test/java/AllDayEventTest.java
+++ src/test/java/AllDayEventTest.java
@@ -135,6 +135,70 @@ public class AllDayEventTest extends TestBase {
 
             assertEquals(Integer.parseInt(allDayCountAfter), Integer.parseInt(allDayCountBefore) + 1);
         }
+
+        @Test
+        public void 미래_올데이_이벤트_생성() {
+            // 미래 일자로 이동해 기존 올데이 이벤트 개수 파악 및 저장
+            for (int i = 0; i < 3; i++) {
+                swipeLeft();
+            }
+
+            allDayCountBefore = getCurAllDayCount();
+
+            // 다시 현재로 복귀
+            for (int i = 0; i < 3; i++) {
+                swipeRight();
+            }
+
+            touchTimeLineLeftSpace();
+
+            WebElement allDay = driver.findElement(xpath("//XCUIElementTypeButton[@name=\"calendar\"]"));
+            allDay.click();
+
+            newEvent = driver.findElement(accessibilityId("새로운 이벤트"));
+            newEvent.sendKeys("미래 올데이 이벤트 생성");
+
+            comment = driver.findElement(xpath("//XCUIElementTypeTextView"));
+            comment.sendKeys("미래 올데이 이벤트 설명 추가");
+
+            // 과거 일자로 조정
+            String specificFutureDate = getSpecificDate(3);
+            startDatePicker = getStartDatePicker();
+            startDatePicker.click();
+
+            String futureDateXpath = "//XCUIElementTypeButton[contains(@name, '" + specificFutureDate + "')]";
+            WebElement futureDate = driver.findElement(xpath(futureDateXpath));
+
+            futureDate.click();
+
+            touchEventBlankSpace();
+
+            endDatePicker = getEndDatePicker();
+            endDatePicker.click();
+
+            WebElement futureEndDate = driver.findElement(xpath(futureDateXpath));
+            futureEndDate.click();
+
+            touchEventBlankSpace();
+
+            check = driver.findElement(accessibilityId("checkmark"));
+            check.click();
+
+            // 미래 위치로 이동
+            for (int i = 0; i < 3; i++) {
+                swipeLeft();
+            }
+
+            // 올데이 이벤트 생성 검증
+            WebElement allDayEventStorage = driver.findElement(xpath("(//XCUIElementTypeImage[@name=\"calendar\"])[1]"));
+            allDayEventStorage.click();
+
+            driver.findElement(accessibilityId("미래 올데이 이벤트 생성"));
+
+            allDayCountAfter = getCurAllDayCount();
+
+            assertEquals(Integer.parseInt(allDayCountAfter), Integer.parseInt(allDayCountBefore) + 1);
+        }
     }
 
     private String getCurAllDayCount() {
