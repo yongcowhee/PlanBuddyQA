5254c796f05a13c687ea3a08eb81ac5884f8bf12
heecong <yongcowhee@gmail.com>
2025-11-05 23:44:04 +0900

    feat: tess4j 의존성 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.testng:testng:7.9.0")
-    implementation("io.appium:java-client:9.1.0")
+    implementation("io.appium:java-client:10.0.0")
+    testImplementation("net.sourceforge.tess4j:tess4j:5.16.0")
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.38.0")
 }
 
 tasks.test {
